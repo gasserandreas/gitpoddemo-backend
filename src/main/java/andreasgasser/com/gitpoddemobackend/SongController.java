360ff1cbf84a550232a3132120e749a117957be5
Andreas Liistro <gasserandreas@users.noreply.github.com>
2023-02-25 22:31:26 +0000

    task: added cors

--- src/main/java/andreasgasser/com/gitpoddemobackend/SongController.java
+++ src/main/java/andreasgasser/com/gitpoddemobackend/SongController.java
@@ -16,23 +16,27 @@ public class SongController {
     // Aggregate root
     // tag::get-aggregate-root[]
     @GetMapping("/songs")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<Song> list() {
         return repository.findAll();
     }
     // end::get-aggregate-root[]
 
     @PostMapping("/songs")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Song post(@RequestBody Song song) {
         return repository.save(song);
     }
 
     @GetMapping("/songs/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Song get(@PathVariable Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new SongNotFoundException(id));
     }
 
     @PutMapping("/songs/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Song put(@RequestBody Song newSong, @PathVariable long id) {
         return repository.findById(id)
                 .map((song) -> {
@@ -47,6 +51,7 @@ public class SongController {
     }
 
     @DeleteMapping("songs/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
     public void delete(@PathVariable long id) {
         repository.deleteById(id);
     }
